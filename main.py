b121748868a90ded6686fd6443db171ceebf6902
huoyiming <dghuoyiming@163.com>
2021-12-10 23:33:11 +0800

    fix

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ list=[]#初始化抽奖列表
 strict_mode='on'#默认开启严格模式
 #尝试读取n2n.json（名单字典），成功则设定error为False，反之亦然
 try:
-    file=open("n2n.json",'r',encoding='utf8')
-    n2n = json.load(file)
-    error='False'
+	file=open("n2n.json",'r',encoding='utf8')
+	n2n = json.load(file)
+	error='False'
 except:
-    print('跳过读取名单')
-    error='True'
+	print('跳过读取名单')
+	error='True'
 
-num=input('中奖人数：')
+num=input('中奖人数:')
 while True:
 	a=input('学号:')
 	#严格模式开启、关闭
@@ -48,18 +48,18 @@ while True:
 			#判断输入值是否在字典中存在
 			if (a in n2n) == True:
 				list.append(a)
-				print('已添加：'+a+n2n[a])
+				print('已添加:'+a+n2n[a])
 			else:
 				list.append(a)
-				print('已添加：'+a)
+				print('已添加:'+a)
 			
 print('恭喜这'+num+'位同学')
 for i in range(int(num)):
-    lucky=random.choice(list)
-    list.remove(lucky)
-    if error == 'False':
-        print(lucky+n2n[lucky])
-    else:
-        print(lucky)
+	lucky=random.choice(list)
+	list.remove(lucky)
+	if error == 'False':
+		print(lucky+n2n[lucky])
+	else:
+		print(lucky)
 
 exit=input('按回车退出')
